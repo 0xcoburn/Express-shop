f41a1e80185c12d6f64d97f1e4d4f168b3d599c0
Matt Coburn <mattcoburn24@gmail.com>
2020-04-04 01:08:27 -0700

    implementing express-handlebars
    replaced pug with express-handlebars and got add-product, shop, and error wired up to a layout page

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+	'hbs',
+	expressHbs({
+		layoutsDir: 'views/layout/',
+		defaultLayout: 'main-layout',
+		extname: 'hbs'
+	})
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).render('error', { pageTitle: 'Page Not Found' });
+	res.status(404).render('error', { pageTitle: 'Page Not Found!' });
 });
 
 app.listen(5000);
