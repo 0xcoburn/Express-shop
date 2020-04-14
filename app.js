43707e3726ca2d9a528759f785249451c4f9b573
Matt Coburn <mattcoburn24@gmail.com>
2020-04-13 20:34:39 -0700

    User Schema and relation to products
    set up User schema and allowed user id to be stored with a product when it is added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-// 	User.findById('5e92c7283ed072d762790760')
-// 		.then((user) => {
-// 			req.user = new User(user.name, user.email, user.cart, user._id);
-// 			next();
-// 		})
-// 		.catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+	User.findById('5e952c99bfd04d249d7f2657')
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,18 @@ mongoose
 		'mongodb+srv://coburn24:jo9zf2mqCZYKnuVw@cluster0-xppk5.mongodb.net/shop?retryWrites=true&w=majority'
 	)
 	.then((result) => {
+		User.findOne().then((user) => {
+			if (!user) {
+				const user = new User({
+					name: 'matt',
+					email: 'matt@test.com',
+					cart: {
+						items: []
+					}
+				});
+				user.save();
+			}
+		});
 		app.listen(3000);
 	})
 	.catch((err) => {
--- models/user.js
+++ models/user.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		require: true
+	},
+	Cart: {
+		items: [
+			{
+				productId: {
+					type: Schema.Types.ObjectId,
+					ref: 'Product',
+					required: true
+				},
+				quantity: { type: Number, require: true }
+			}
+		]
+	}
+});
+
+module.exports = mongoose.model('User', userSchema);
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
