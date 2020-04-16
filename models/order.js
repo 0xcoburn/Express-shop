57537c57d26e67092b34bfd169b028ca0469192b
Matt <mattcoburn24@gmail.com>
2020-04-16 02:24:42 -0500

    adding sessions to the app (#1)
    * Setup express-session
    
    so users can be tracked and sessions are stored in mongodb. User model and methods are still attached to req.user
    
    * mobile

--- models/order.js
+++ models/order.js
@@ -1,23 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    products: [{
-        productData: { type: Object, require: true},
+	products: [
+		{
+			product: { type: Object, require: true },
 
-        quantity { type: number, require: true}
-    }],
-    user: { 
-        name: {
-            type: String,
-            require: true
-        }},
-        userId: {
-            type: Schema.Types.ObjectId,
-            required: true,
-            ref: 'User'
-        }
-})
+			quantity: { type: Number, require: true }
+		}
+	],
+	user: {
+		name: {
+			type: String,
+			require: true
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User'
+		}
+	}
+});
 
-module.exports = mongoose.model('Order', orderSchema)
+module.exports = mongoose.model('Order', orderSchema);
